88dc47241a9ff1f92e65aad58bafadd649d7ddc1
Rimuru-Tempest-26 <pkh2662003@gmail.com>
2023-03-21 00:22:46 +0630

    finish

--- app.js
+++ app.js
@@ -3,6 +3,44 @@ const bookList = document.querySelector('#bookList');
 
 class BookStore {
   books = [];
+
+  title = '';
+
+  author = '';
+
+  set title(title) {
+    this.title = title;
+  }
+
+  set author(author) {
+    this.author = author;
+  }
+
+  showBookList(bookList) {
+    bookList.innerHTML = '';
+
+    this.books.forEach((b, index) => {
+      bookList.innerHTML += `
+        <tr class="p-2 align-middle">
+          <td>"${b.title}" by ${b.author}</td>
+          <td><button class="btn btn-danger float float-end" type="button" onclick="remove(${index})">Remove</button></td>
+        </tr>
+    `;
+    });
+  }
+
+  addBook(bookList) {
+    this.books.push({ title: this.title, author: this.author });
+    localStorage.setItem('books', JSON.stringify(this.books));
+    bookForm.reset();
+    this.showBookList(bookList);
+  }
+
+  removeBook(id, bookList) {
+    this.books.splice(id, 1);
+    localStorage.setItem('books', JSON.stringify(this.books));
+    this.showBookList(bookList);
+  }
 }
 
 const book = new BookStore();
@@ -13,39 +51,17 @@ if (localStorage.getItem('books') !== null) {
   book.books = [];
 }
 
-function showBookList() {
-  bookList.innerHTML = '';
-
-  book.books.forEach((b, index) => {
-    bookList.innerHTML += `
-        <p>${b.title}</p>
-        <p>${b.author}</p>
-        <button type="button" onclick="removeBook(${index})">Remove</button>
-        <hr>
-    `;
-  });
-}
-
-function addBook(title, author) {
-  book.books.push({ title, author });
-  localStorage.setItem('books', JSON.stringify(book.books));
-  bookForm.reset();
-  showBookList();
-}
-
-function removeBook(id) {
-  book.books.splice(id, 1);
-  localStorage.setItem('books', JSON.stringify(book.books));
-  showBookList();
-}
-
 bookForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  const title = bookForm.title.value;
-  const author = bookForm.author.value;
-  if (title !== '' && author !== '') {
-    addBook(title, author);
+  book.title = bookForm.title.value;
+  book.author = bookForm.author.value;
+  if (book.title !== '' && book.author !== '') {
+    book.addBook(bookList);
   }
 });
 
-showBookList();
+function remove(id) {
+  book.removeBook(id, bookList);
+}
+
+book.showBookList(bookList);
